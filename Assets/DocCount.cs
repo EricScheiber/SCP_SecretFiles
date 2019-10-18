7f9db7c05e584c40b98d15477522d3f67c092185
Scheiber <alien69eric@hotmail.fr>
2019-10-18 13:37:19 +0200

    Menu

---
+++ Assets/DocCount.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DocCount : MonoBehaviour
+{
+
+
+    int DocFound = 0;
+
+
+    void Update()
+    {
+        /*
+         if (DocFound <= 1){
+         
+        Bouton Doc1 est disponible 
+
+         }
+
+        if (DocFound <= 2){
+         
+        Bouton Doc2 est disponible 
+
+         }
+
+        if (DocFound <= 3){
+         
+        Bouton Doc3 est disponible 
+
+         }
+
+        if (DocFound <= 4){
+         
+        Bouton Doc4 est disponible 
+
+         }
+         * */
+    }
+}

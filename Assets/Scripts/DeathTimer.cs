d1134ce58c153602f5902ac33159e518cf1ee3f7
Vincent <vincent.givaudan@hotmail.fr>
2019-10-31 16:41:54 +0100

    Début death timer

---
+++ Assets/Scripts/DeathTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTimer : MonoBehaviour
+{
+    public float DeathTime;
+
+    private CharacterController controller;
+    public float currentTime;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        currentTime = DeathTime;
+    }
+
+    void Update()
+    {
+        if (controller.velocity.magnitude == 0f)
+        {
+            currentTime -= Time.deltaTime;
+        }
+    }
+}
